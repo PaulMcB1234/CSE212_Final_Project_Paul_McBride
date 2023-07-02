2b38bc6a8588787050fa971ca20e3876c658c516
PaulMcB1234 <113477302+PaulMcB1234@users.noreply.github.com>
2023-07-01 20:21:07 -0600

    Create Problem_1_solution.py

---
+++ Problem_1_solution.py
@@ -0,0 +1,29 @@
+def is_available(order):
+    if order == "plain":
+        return True
+    elif order == "bluberry":
+        return True
+    elif order == "chocolate":
+        return True
+    else:
+        return False
+    
+def pancake_orders(orders):
+    stack = []
+    for order in orders:
+        if is_available(order):
+            stack.append(order)
+            print("Order up!")
+        elif is_available(order) == False:
+            print("That item is not on the menu.")
+    
+    return stack
+
+orders = ["plain", "bluberry", "chocolate"]
+print(pancake_orders(orders))
+print()
+orders = ["bluberry", "apple", "plain", "chocolate", "chocolate"]
+print(pancake_orders(orders))
+print()
+orders = ["gluten free", "plain", "plain", "chocolate", "bluberry", "mango", "large", "plain", "large",]
+print(pancake_orders(orders))
